bac3983d634c9769faf8ab99a05606fe16d412b5
dean06001 <dean06001@E-PW0234KC>
2023-10-06 13:22:32 +0200

    Add project files.

---
+++ inlämnings uppgift 3.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace inlämnings_uppgift_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många minuter lång är din låt");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("hur många sekunder lång är din låt");
+            int sekunder = int.Parse(Console.ReadLine());
+            if ((minuter >= 2 && sekunder >= 45) || (minuter <= 4 && sekunder <= 20))
+            {
+                Console.WriteLine("Din kan spelas på radion");
+            }
+            else if ((minuter >= 4 && sekunder >= 20) || (minuter > 4 && sekunder <= 20))
+            {
+                Console.WriteLine("din låt får inte spelas");
+            }
+            else
+            {
+                Console.WriteLine("din låt är för kort");
+            }
+            Console.ReadKey();
+        }
+    }
+}
